673e32df4fba6409f00b9a964168e55e6f3c7ff7
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-17 21:50:16 -0300

    removi alguns arquivos desnecessários

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
-const card = []
+const HTMLcard = []
 
-//card conterá 12 objetos com 2 propriedades (conteiner da carta,img da carta)
+//card conterá 12 objetos com 2 propriedades (conteiner da carta,tag img da carta)
 for (let pos in document.getElementsByClassName('card')){
-    card.push({conteiner:document.getElementsByClassName('card')[pos],img:document.getElementsByTagName('img')[pos]})
+    HTMLcard.push({conteiner:document.getElementsByClassName('card')[pos],Tagimg:document.getElementsByTagName('img')[pos]})
 }
 
-//Adicionando listeners nos conteiners das cartas
-for (let pos in card) {
-    card[pos].conteiner.addEventListener('click',GeneralFunction)
-}
+const card = [
+    'images/acordeon.jpg',
+    'images/bateria.jpg',
+    'images/guitarra.jpg',
+    'images/piano.jpg',
+    'images/violao.jpg',
+    'images/violino.jpg',
+    'images/acordeon.jpg',
+    'images/bateria.jpg',
+    'images/guitarra.jpg',
+    'images/piano.jpg',
+    'images/violao.jpg',
+    'images/violino.jpg'     
+]
 
-function GeneralFunction() {
+const returnedNumbers = []
+let randomNumber = null
+
+function generateRandomNumber() {
+    //Gerar número aletório entre 0 e 11
+
+    let randomNumber = Math.floor(12*Math.random())
+    
+    if (returnedNumbers.indexOf(randomNumber) == -1) {
+        returnedNumbers.push(randomNumber)
+        return randomNumber
+    } 
     
 }
+
+
+for (let pos in HTMLcard) {
+    // do {
+    //     randomNumber = generateRandomNumber()
+    // } while (randomNumber == undefined)
+
+    //Não sei como vou gerar uma sequência de 12 números aleatórios sem forçar a memória do pc
+
+    HTMLcard[pos].Tagimg.setAttribute('src',card[pos])
+}
+ 
 
 
